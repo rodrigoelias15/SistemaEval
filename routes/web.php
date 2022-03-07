2dc2603573665d1419955639be5f52262e055140
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-03-06 22:19:56 -0300

    laravel 8.x

--- routes/web.php
+++ routes/web.php
@@ -13,55 +13,57 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\RegistroController;
 use App\Http\Controllers\EntrarController;
+use App\Http\Controllers\SiteController;
 
 // Página Inicial
-Route::get('/home_sistema', 'SiteController@home_sistema')->name('home_avaliador');
+Route::get('/home_sistema', [SiteController::class, 'home_sistema'])->name('home_avaliador');
 
 // Questionário e cadastro questionário
-Route::get('/questionario', 'SiteController@questionario')->name('questionario');
-Route::post('/questionario', 'SiteController@storeQuestionario');
+Route::get('/questionario', [SiteController::class,'questionario'])->name('questionario');
+Route::post('/questionario', [SiteController::class,'storeQuestionario']);
 
 // Página Pré Questionário
-Route::get('/prequestionario', 'SiteController@preQuestionario')->name('pre_questionario');
-Route::post('/prequestionario', 'SiteController@postPreQuestionario');
+Route::get('/prequestionario', [SiteController::class,'preQuestionario'])->name('pre_questionario');
+Route::post('/prequestionario', [SiteController::class,'postPreQuestionario']);
 
 // Cadastro Admin
-Route::get('/cadastroadmin', 'SiteController@cadastroadmin')->name('cadastro_admin');
+Route::get('/cadastroadmin', [SiteController::class,'cadastroadmin'])->name('cadastro_admin');
 
 // Cadastro Avaliador
-Route::get('/cadastroavaliador', 'SiteController@cadastroavaliador')->name('cadastro_avaliador');
-Route::post('/cadastroavaliador', 'SiteController@cadastroavaliador_bancodados');
+Route::get('/cadastroavaliador', [SiteController::class,'cadastroavaliador'])->name('cadastro_avaliador');
+Route::post('/cadastroavaliador', [SiteController::class,'cadastroavaliador_bancodados']);
 
 // Cadastro Itens Digitais
-Route::get('/cadastroitemdigital', 'SiteController@cadastroitemdigital')->name('cadastro_itemdigital');
-Route::post('/cadastroitemdigital', 'SiteController@cadastroItemDigital_bancodados');
+Route::get('/cadastroitemdigital', [SiteController::class,'cadastroitemdigital'])->name('cadastro_itemdigital');
+Route::post('/cadastroitemdigital', [SiteController::class,'cadastroItemDigital_bancodados']);
 
 // Cadastro Instituição
-Route::get('/cadastroinstituicao', 'SiteController@cadastroinstituicao')->name('cadastro_instituicao');
-Route::post('/cadastroinstituicao', 'SiteController@cadastroinstituicao_bancodados');
+Route::get('/cadastroinstituicao', [SiteController::class,'cadastroinstituicao'])->name('cadastro_instituicao');
+Route::post('/cadastroinstituicao', [SiteController::class,'cadastroinstituicao_bancodados']);
 
 // Teste de Cadastrado
-Route::get('/testecadastroavaliador', 'SiteController@testeCadastroAvaliador')->name('teste_cadastro_avaliador');
-Route::get('/testecadastroinstituicao', 'SiteController@testeCadastroInstituicao')->name('teste_cadastro_instituicao');
-Route::get('/testecadastroitemdigital', 'SiteController@testeCadastroItensDigitais')->name('teste_cadastro_item_digital');
-Route::get('/testecadastroquestionario', 'SiteController@testeCadastroQuestionario')->name('testeCadastroQuestionario');
+Route::get('/testecadastroavaliador', [SiteController::class,'testeCadastroAvaliador'])->name('teste_cadastro_avaliador');
+Route::get('/testecadastroinstituicao', [SiteController::class, 'testeCadastroInstituicao'])->name('teste_cadastro_instituicao');
+Route::get('/testecadastroitemdigital', [SiteController::class,'testeCadastroItensDigitais'])->name('teste_cadastro_item_digital');
+Route::get('/testecadastroquestionario', [SiteController::class, 'testeCadastroQuestionario'])->name('testeCadastroQuestionario');
 
 // Contato com Administrador
-Route::get('/contatoadmin', 'SiteController@contatoadmin')->name('contato_admin');
+Route::get('/contatoadmin', [SiteController::class, 'contatoadmin'])->name('contato_admin');
 
 // Contato com Avaliador
-Route::get('/contatoavaliador', 'SiteController@contatoavaliador')->name('contato_avaliador');
+Route::get('/contatoavaliador', [SiteController::class, 'contatoavaliador'])->name('contato_avaliador');
 
 // Autenticação
-// Route::get('/', 'EntrarController@login')->name('login_sistema');
+// Route::get('/', 'EntrarController::class,login')->name('login_sistema');
 Route::get('/', [EntrarController::class, 'login'])->name('login_sistema');
-Route::post('/', 'EntrarController@entrar');
+Route::post('/', [EntrarController::class, 'entrar']);
 
 
 // Registro de Login
-Route::get('/registrar', 'RegistroController@create');
-Route::post('/registrar', 'RegistroController@store');
+Route::get('/registrar', [RegistroController::class,'create']);
+Route::post('/registrar', [RegistroController::class,'store']);
 
 // Logout
 Route::get('/sair', function () {
@@ -70,5 +72,5 @@ Route::get('/sair', function () {
 });
 
 // Autenticação padrão do laravel
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class,'index'])->name('home');
 Auth::routes();
