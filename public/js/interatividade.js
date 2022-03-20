b594627145cb337dc707b3ad9b4d9e40184e9c91
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-03-20 09:01:33 -0300

    script de interatividade

--- public/js/interatividade.js
+++ public/js/interatividade.js
@@ -1,10 +1,11 @@
 
-let soma = 0;
+// let somaTotal = 0;
 
 function indicador1() {
     let baixa = 0;
     let media = 0;
     let alta = 0;
+    let somatorio = 0;
     var item_a = document.getElementById("indicador1a");
     var item_b = document.getElementById("indicador1b");
     var item_c = document.getElementById("indicador1c");
@@ -23,14 +24,20 @@ function indicador1() {
         baixa = 0;
         media = 0;
     }
-    soma = baixa + media + alta;   
-    return soma;
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
 }
 
 function indicador2() {
     let baixa = 0;
     let media = 0;
     let alta = 0;
+    let somatorio = 0;
     var item_a = document.getElementById("indicador2a");
     var item_b = document.getElementById("indicador2b");
     var item_c = document.getElementById("indicador2c");
@@ -49,24 +56,313 @@ function indicador2() {
         baixa = 0;
         media = 0;
     }
-    soma = baixa + media + alta;
-    return soma;
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
 }
 
-function somatorio() {
-    return indicador1() + indicador2();
+function indicador3() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador3a");
+    var item_b = document.getElementById("indicador3b");
+    var item_c = document.getElementById("indicador3c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
 }
 
-var somatoriovar = somatorio();
-
-document.getElementById("count").innerHTML = somatoriovar;
-
-if (somatoriovar < 40){
-    document.getElementById("teste").innerHTML = "Baixa Interatividade";
+function indicador4() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador4a");
+    var item_b = document.getElementById("indicador4b");
+    var item_c = document.getElementById("indicador4c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
 }
-else if (somatoriovar >= 40 && somatoriovar < 70){
-    document.getElementById("teste").innerHTML = "Média Interatividade";
+
+function indicador5() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador5a");
+    var item_b = document.getElementById("indicador5b");
+    var item_c = document.getElementById("indicador5c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
 }
-else if (somatoriovar >= 70 && somatoriovar <= 100){
-    document.getElementById("teste").innerHTML = "Alta Interatividade";
+
+function indicador6() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador6a");
+    var item_b = document.getElementById("indicador6b");
+    var item_c = document.getElementById("indicador6c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function indicador7() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador7a");
+    var item_b = document.getElementById("indicador7b");
+    var item_c = document.getElementById("indicador7c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function indicador8() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador8a");
+    var item_b = document.getElementById("indicador8b");
+    var item_c = document.getElementById("indicador8c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function indicador9() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador9a");
+    var item_b = document.getElementById("indicador9b");
+    var item_c = document.getElementById("indicador9c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function indicador10() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador10a");
+    var item_b = document.getElementById("indicador10b");
+    var item_c = document.getElementById("indicador10c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function indicador11() {
+    let baixa = 0;
+    let media = 0;
+    let alta = 0;
+    let somatorio = 0;
+    var item_a = document.getElementById("indicador11a");
+    var item_b = document.getElementById("indicador11b");
+    var item_c = document.getElementById("indicador11c");
+    if (item_a.checked == true && item_b.checked == false && item_c.checked == false) {
+        baixa = 5;
+        media = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == true && item_c.checked == false) {
+        media = 10;
+        baixa = 0;
+        alta = 0;
+    }
+    else if (item_a.checked == false && item_b.checked == false && item_c.checked == true) {
+        alta = 15;
+        baixa = 0;
+        media = 0;
+    }
+    somatorio = baixa + media + alta;
+    if (somatorio == 5 || somatorio == 10 || somatorio == 15) {
+        return somatorio;
+    }
+    else {
+        return 0;
+    }
+}
+
+function somaInteratividade() {
+    var somaTotal = indicador1() + indicador2() + indicador3() + indicador4() + indicador5() + indicador6() + indicador7() + indicador8() + indicador9() + indicador10() + indicador11();
+    if (somaTotal < 40) {
+        document.getElementById("teste").innerHTML = "Baixa Interatividade";
+    }
+    else if (somaTotal >= 40 && somaTotal < 70) {
+        document.getElementById("teste").innerHTML = "Média Interatividade";
+    }
+    else if (somaTotal >= 70 && somaTotal <= 100) {
+        document.getElementById("teste").innerHTML = "Alta Interatividade";
+    }
+    document.getElementById("count").innerHTML = somaTotal;
 }
