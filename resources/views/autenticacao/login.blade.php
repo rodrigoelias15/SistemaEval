6a101f20a2641906110140ab76158849d33f0e8a
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-03-29 19:37:14 -0300

    Ajeitado layout do login e cadastro de usuario

--- resources/views/autenticacao/login.blade.php
+++ resources/views/autenticacao/login.blade.php
@@ -7,58 +7,61 @@ Login
 @section("content")
 
 @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<div class="row justify-content-center">
+    <div class="col-3">
+        <div class="logo d-flex mt-4">
+            <img class="img-fluid" src="img/caedlogo.png" alt="Logo Caed">
         </div>
-    @endif
 
-<div class="my-5 d-flex flex-column align-items-center">
-    <div class="logo">
-        <img class="img-fluid" src="img/caedlogo.png" alt="Logo Caed">
-    </div>
-    <div class="formulario">
-        <form method="post">
-            @csrf
-            <div class="form-group">
-                <label>Usuário</label>
-                <input class="form-control" type="text" name="email">
-            </div>
-            <div class="form-group">
-                <label>Senha</label>
-                <div class="input-group align-items-center">
-                    <input id="mostrar_senha" class="form-control" type="password" name="password">
-                    <i id="mostrar_senha_icon" class="fas fa-eye mr-2" onclick="mostrarSenha()"></i>
+        <div class="formulario">
+            <form method="post">
+                @csrf
+                <div class="form-group">
+                    <label>E-mail</label>
+                    <input class="form-control" type="text" name="email">
                 </div>
-            </div>
+                <div class="form-group">
+                    <label>Senha</label>
+                    <div class="input-group align-items-center">
+                        <input id="mostrar_senha" class="form-control" type="password" name="password">
+                        <i id="mostrar_senha_icon" class="fas fa-eye mr-2" onclick="mostrarSenha()"></i>
+                    </div>
+                </div>
 
-            <script type="text/javascript">
-                function mostrarSenha(){
-                    if($('#mostrar_senha_icon').hasClass('fas fa-eye mr-2')){
-                        $('#mostrar_senha_icon').removeClass('fas fa-eye mr-2').addClass('fas fa-eye-slash mr-2');
-                        $('#mostrar_senha').attr("type", "text");
-                    }else{
-                        $('#mostrar_senha_icon').removeClass('fas fa-eye-slash mr-2').addClass('fas fa-eye mr-2');
-                        $('#mostrar_senha').attr("type", "password");
-                    }
-                };
-            </script>
+                <script type="text/javascript">
+                    function mostrarSenha(){
+                        if($('#mostrar_senha_icon').hasClass('fas fa-eye mr-2')){
+                            $('#mostrar_senha_icon').removeClass('fas fa-eye mr-2').addClass('fas fa-eye-slash mr-2');
+                            $('#mostrar_senha').attr("type", "text");
+                        }else{
+                            $('#mostrar_senha_icon').removeClass('fas fa-eye-slash mr-2').addClass('fas fa-eye mr-2');
+                            $('#mostrar_senha').attr("type", "password");
+                        }
+                    };
+                </script>
 
-            <div class="form-group">
-                <input class="btn btn-primary w-100 mt-2" type="submit" value="Entrar">
-                <a href="/registrar" class="btn btn-primary w-100 mt-2">Criar Conta</a>
+                <div class="form-group">
+                    <input class="btn btn-primary w-100 mt-2" type="submit" value="Entrar">
+                    <a href="/registrar" class="btn btn-primary w-100 mt-2">Criar Conta</a>
+                </div>
+                <div class="form-group">
+                    <a href="#" target="_blank">Esqueci minha senha</a>
+                </div>
+            </form>
+            <div class="text-right" style="font-style: italic; font-size: 14px">
+                <span>
+                    v1.7.1
+                </span>
             </div>
-            <div class="form-group">
-                <a href="#" target="_blank">Esqueci minha senha</a>
-            </div>
-        </form>
-        <div class="text-right" style="font-style: italic; font-size: 14px">
-            <span>
-                v1.7
-            </span>
         </div>
     </div>
 </div>
--- resources/views/registro/create.blade.php
+++ resources/views/registro/create.blade.php
@@ -5,44 +5,60 @@ Novo Registro
 @endsection
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-5">
-        <div class="logo d-flex mt-4">
-            <img class="img-fluid" src="img/caedlogo.png" alt="Logo Caed">
-        </div>
 
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-        @endif
-
-        <form method="post">
-            @csrf
-            <div class="form-group">
-                <label for="name">Nome</label>
-                <input type="text" name="name" id="name" required class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="email">E-mail</label>
-                <input type="email" name="email" id="email" required class="form-control">
-            </div>
-
-            <div class="form-group">
-                <label for="password">Senha</label>
-                <input type="password" name="password" id="password" required min="1" class="form-control">
-            </div>
-
-            <div class="form-group text-right mt-3">
-                <a href="/" class="btn btn-primary mr-2">Voltar</a>
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </div>
-        </form>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
     </div>
-</div>
+    @endif
+    
+    <div class="row justify-content-center">
+        <div class="col-3">
+            <div class="logo d-flex mt-4">
+                <img class="img-fluid" src="img/caedlogo.png" alt="Logo Caed">
+            </div>
+
+            <form method="post">
+                @csrf
+                <div class="form-group">
+                    <label for="name">Nome</label>
+                    <input type="text" name="name" id="name" required class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email" required class="form-control">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Senha</label>
+                    <div class="input-group align-items-center">
+                        <input type="password" name="password" id="password" required min="1" class="form-control">
+                        <i id="mostrar_senha_icon" class="fas fa-eye mr-2" onclick="mostrarSenha()"></i>
+                    </div>
+                </div>
+
+                <script type="text/javascript">
+                    function mostrarSenha(){
+                        if($('#mostrar_senha_icon').hasClass('fas fa-eye mr-2')){
+                            $('#mostrar_senha_icon').removeClass('fas fa-eye mr-2').addClass('fas fa-eye-slash mr-2');
+                            $('#password').attr("type", "text");
+                        }else{
+                            $('#mostrar_senha_icon').removeClass('fas fa-eye-slash mr-2').addClass('fas fa-eye mr-2');
+                            $('#password').attr("type", "password");
+                        }
+                    };
+                </script>
+
+                <div class="form-group text-right mt-3">
+                    <button type="submit" class="btn btn-primary w-100">Cadastrar</button>
+                    <a href="/" class="btn btn-primary w-100 my-2">Voltar</a>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
