4764e5b15ee354bb0ff6f7a7939188c921ab5128
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-04-01 19:00:05 -0300

    Versão atualizada na página login

--- resources/views/autenticacao/login.blade.php
+++ resources/views/autenticacao/login.blade.php
@@ -59,7 +59,7 @@ Login
             </form>
             <div class="text-right" style="font-style: italic; font-size: 14px">
                 <span>
-                    v1.7.1
+                    v1.8.1
                 </span>
             </div>
         </div>
