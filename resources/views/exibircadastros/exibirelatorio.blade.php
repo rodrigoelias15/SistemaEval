d15abc07f50f2fc2eec3007cef9e4b4e7f494532
Rodrigo <elias.rodrigo@estudante.ufjf.br>
2022-10-07 13:55:28 -0300

    Criada opção de ordenar relatórios por nome e data

--- resources/views/exibircadastros/exibirelatorio.blade.php
+++ resources/views/exibircadastros/exibirelatorio.blade.php
@@ -1,113 +1,33 @@
 @extends('layouts.layout')
 
 @section('titulo_pagina')
-Visualizar Relatório do Questionário
+    Visualizar Relatório do Questionário
 @endsection
 
 @section('content')
-   
-
     <div class="row">
-        @foreach($relatorio_questionario as $questionario)
-        <div class="col-sm-6 my-3">
-            <div class="card">        
-                <div class="card-body">            
-                    <p><span class="cabecalho"> Título do Item: </span> {{$questionario->nome_item_digital_questionario}} </p>
-                    <p><span class="cabecalho"> Nome do Avaliador: </span> {{$questionario->nome_avaliador_questionario}} </p>
-                    <p><span class="cabecalho"> Data da Avaliação: </span> {{ (new DateTime($questionario->data_item_digital_questionario))->format('d/m/Y')}} </p>
-                
-                    <section id="indicador1" class="my-3">
-                        <h1 class="indicador" onclick="somaInteratividade()">Indicador 1: Textos e elementos gráficos</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item1 ?></p>                    
-                    </section>
-                
-                    <section id="indicador2" class="my-3">
-                        <h1 class="indicador">Indicador 2: Espaço/meio de interação</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item2 ?></p>
-                    </section>
-                
-                    <section id="indicador3" class="my-3">
-                        <h1 class="indicador">Indicador 3: Comportamento dos elementos no espaço</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item3 ?></p>                    
-                    </section>
-                
-                    <section id="indicador4" class="my-3">
-                        <h1 class="indicador">Indicador 4: Movimento/liberdade do usuário no item</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item4 ?></p>                    
-                    </section>
-                
-                    <section id="indicador5" class="my-3">
-                        <h1 class="indicador">Indicador 5: Suporte e feedback</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item5 ?></p>                    
-                    </section>
-                
-                    <section id="indicador6" class="my-3">
-                        <h1 class="indicador">Indicador 6: Diálogo entre usuário e conteúdo</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item6 ?></p>                    
-                    </section>
-                
-                    <div class="page-break"></div>
-                
-                    <section id="indicador7" class="my-3">
-                        <h1 class="indicador">Indicador 7: Flexibilidade e abertura</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item7 ?></p>                    
-                    </section>
-                
-                    <section id="indicador8" class="my-3">
-                        <h1 class="indicador">Indicador 8: Integração</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item8 ?></p>                    
-                    </section>
-                
-                    <section id="indicador9" class="my-3">
-                        <h1 class="indicador">Indicador 9: Permutabilidade</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item9 ?></p>                    
-                    </section>
-                
-                    <section id="indicador10" class="my-3">
-                        <h1 class="indicador">Indicador 10: Intervenção</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item10 ?></p>                    
-                    </section>
-                
-                    <section id="indicador11" class="my-3">
-                        <h1 class="indicador">Indicador 11: Bidirecionalidade</h1>
-                        <h2 class="criterio">Critério escolhido:</h2>                    
-                        <p><?php echo $questionario->questionario_item11 ?></p>                    
-                    </section>
-                </div>
-                <form class="text-center mb-3" method="post" action="/relatorio/remover/{{$questionario->id}}" onsubmit="return confirm('Deseja excluir o Relatório do Item Digital {{$questionario->nome_item_digital_questionario}}?')">
-                    @csrf
-                    <button class="btn btn-danger px-4" type="submit">Excluir</button>
-                </form>
-            </div>
-        </div>
-        @endforeach 
+        <select class="custom-select" name="ordenaRelatorio" id="ordena">
+            <option selected>Escolha uma ordenação</option>
+            <option value="1">
+                Ordena por nome
+            </option>
+            <option value="2">
+                Ordena por data
+            </option>
+        </select>
+        <button class="btn btn-primary" id="btn">Ordenar</button>
     </div>
 
-    <style>
-        .page-break {
-            page-break-after: always;
-        }
-        .indicador{
-            font-size: 16px;
-            font-weight: bold;
-        }
-        .criterio{
-            font-size: 14px;
-            font-weight: bold;
-        }
-        .cabecalho{
-            font-weight: bold;
-        }
-    </style>
-
+    <script type="text/javascript">
+        const btn = document.querySelector('#btn');
+        const ordena = document.querySelector('#ordena');
+        btn.onclick = (event) => {
+            if(ordena.selectedIndex == "1"){
+                window.location.href = "{{route('relatorioOrdenadoNome')}}";
+            }
+            else if(ordena.selectedIndex == "2"){
+                window.location.href = "{{route('relatorioOrdenadoData')}}";
+            }
+        };
+    </script>
 @endsection
