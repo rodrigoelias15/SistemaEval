27150a374eb38f0bba8a8b268f60531cc29a0f90
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-11-18 10:47:46 -0300

    Atualização dos critérios no relatório e chamada da função "exibeNivelInteratividade"

--- resources/views/relatorio.blade.php
+++ resources/views/relatorio.blade.php
@@ -7,7 +7,7 @@
     <p><span class="cabecalho"> Data da Avaliação: </span> {{ (new DateTime($questionario->data_item_digital_questionario))->format('d/m/Y')}} </p>
 
     <section id="indicador1">
-        <h1 class="indicador" onclick="somaInteratividade()">Indicador 1: Textos e elementos gráficos</h1>
+        <h1 class="indicador" onclick="exibeNivelInteratividade()">Indicador 1: Textos e elementos gráficos</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item1 ?></p>                    
     </section>
@@ -25,19 +25,19 @@
     </section>
 
     <section id="indicador4">
-        <h1 class="indicador">Indicador 4: Movimento/liberdade do usuário no item</h1>
+        <h1 class="indicador">Indicador 4: Tempo</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item4 ?></p>                    
     </section>
 
     <section id="indicador5">
-        <h1 class="indicador">Indicador 5: Suporte e feedback</h1>
+        <h1 class="indicador">Indicador 5: Movimento/liberdade do usuário no item</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item5 ?></p>                    
     </section>
 
     <section id="indicador6">
-        <h1 class="indicador">Indicador 6: Diálogo entre usuário e conteúdo</h1>
+        <h1 class="indicador">Indicador 6: Suporte e feedback</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item6 ?></p>                    
     </section>
@@ -45,33 +45,44 @@
     <div class="page-break"></div>
 
     <section id="indicador7">
-        <h1 class="indicador">Indicador 7: Flexibilidade e abertura</h1>
+        <h1 class="indicador">Indicador 7: Diálogo entre usuários e conteúdo</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item7 ?></p>                    
     </section>
 
     <section id="indicador8">
-        <h1 class="indicador">Indicador 8: Integração</h1>
+        <h1 class="indicador">Indicador 8: Flexibilidade e Abertura</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item8 ?></p>                    
     </section>
 
     <section id="indicador9">
-        <h1 class="indicador">Indicador 9: Permutabilidade</h1>
+        <h1 class="indicador">Indicador 9: Integração Inter-áreas</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item9 ?></p>                    
     </section>
 
     <section id="indicador10">
-        <h1 class="indicador">Indicador 10: Intervenção</h1>
+        <h1 class="indicador">Indicador 10: Integração colaborativa</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item10 ?></p>                    
     </section>
 
     <section id="indicador11">
-        <h1 class="indicador">Indicador 11: Bidirecionalidade</h1>
+        <h1 class="indicador">Indicador 11: Permutabilidade</h1>
         <h2 class="criterio">Critério escolhido:</h2>                    
         <p><?php echo $questionario->questionario_item11 ?></p>                    
+    </section>
+
+    <section id="indicador12">
+        <h1 class="indicador">Indicador 12: Intervenção</h1>
+        <h2 class="criterio">Critério escolhido:</h2>                    
+        <p><?php echo $questionario->questionario_item12 ?></p>                    
+    </section>
+    <section id="indicador13">
+        <h1 class="indicador">Indicador 13: Bidirecionalidade</h1>
+        <h2 class="criterio">Critério escolhido:</h2>                    
+        <p><?php echo $questionario->questionario_item13 ?></p>                    
     </section>
 </body>
 
