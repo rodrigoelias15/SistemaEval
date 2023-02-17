9b249b230fb55eb84284731358c8a101f8e80158
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-02-16 21:38:32 -0300

    Mensagem pagina inicial (incompleto)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.layout')
 
 @section('titulo_pagina')
-Página Inicial
+    Página Inicial
 @endsection
 
 @section('content')
-
+    <div class="row justify-content-center">
+        <div class="text-center w-75 mt-4">
+            <section id="texto_apresentacao">
+                O Sistema e-VAL avalia o nível de interatividade nos itens digitais desenvolvidos e utilizados para
+                avaliação da aprendizagem e em avaliações em larga escala. Tem por objetivo auxiliar Instituições,
+                professores e desenvolvedores de itens digitais
+            </section>
+        </div>
+    </div>
 @endsection
