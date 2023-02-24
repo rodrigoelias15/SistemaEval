0748b4dbea62fdd88fc946c8ecd6b5f7c60ede15
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-02-24 14:18:47 -0300

    tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,11 +6,11 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <div class="text-center w-75 mt-4">
-            <section id="texto_apresentacao">
-                O Sistema e-VAL avalia o nível de interatividade nos itens digitais desenvolvidos e utilizados para
-                avaliação da aprendizagem e em avaliações em larga escala. Tem por objetivo auxiliar Instituições,
-                professores e desenvolvedores de itens digitais
+        <div id="texto_apresentacao" class="text-justify mt-4 w-50">
+            <img id="img_inicial" src="img/questionario.jpeg">
+            <section>
+                O Sistema e-VAL tem por objetivo auxiliar instituições, professores e desenvolvedores de itens digitais quanto a seu nível de interatividade, através de um questionário interativo, o sistema calcula a pontuação do item digital que está sendo avaliado, que é mostrado em tempo real.
+                O sistema permite a geração de um relatório com todas as questões respondidas, e caso seja relevante para o usuário, a possibilidade de download.
             </section>
         </div>
     </div>
