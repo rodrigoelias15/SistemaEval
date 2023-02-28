cca8f55eb30d312193929d0858d47e75e09c8cfa
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-02-28 10:28:36 -0300

    Nome da equipe na tela inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,6 +11,15 @@
             <section>
                 O Sistema e-VAL tem por objetivo auxiliar instituições, professores e desenvolvedores de itens digitais quanto a seu nível de interatividade, através de um questionário interativo, o sistema calcula a pontuação do item digital que está sendo avaliado, que é mostrado em tempo real.
                 O sistema permite a geração de um relatório com todas as questões respondidas, e caso o usuário deseje, poderá realizar o download do mesmo.
+                <br>
+                <br>
+
+                Equipe: 
+                Liamara Scortegagna (CAEd/UFJF) <br>
+                Adriana Rocha Bruno (CAEd/UNIRIO) <br>
+                Rodrigo Oliveira Elias (UFJF) <br>
+                Alexandre (UFJF) <br>
+                Projeto de Pesquisa financiado pelo Centro de Políticas Públicas e Avaliação da Educação da Universidade Federal de Juiz de Fora (CAEd/UFJF) <br>
             </section>
         </div>
     </div>
