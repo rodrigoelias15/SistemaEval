2c87f8d3211adb7aae36a80c615346df1f279f17
Alexandre <alexandre.braga@estudante.ufjf.br>
2023-03-13 11:02:53 -0300

    feat: buttons update

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,11 +1,20 @@
 <style>
     .menubutton {
-        background-image: linear-gradient(5deg, #f37335, #fee140);
         color: #101728;
         border-radius: 1rem;
+        background-image: linear-gradient(to right, #f37335 0%, #F09819  51%, #FF512F  100%);
+        text-transform: uppercase;
+        text-align: center;
+        transition: 0.5s;
+        background-size: 200% auto;
+        box-shadow: 0 0 1rem #eee;
     }
-</style>
-
+    .menubutton:hover{
+        background-position: right center; /* change the direction of the change here */
+        color: #fff;
+    }
+</style>   
+         
 <div class="form-group text-center">
     <button class="menubutton btn btn-danger mt-2 px-4" type="submit" style="width:80%">
         {{ $slot }}
