da320fa93b36d4cd881aa26864e7461b6d08fcd1
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-04-26 19:41:20 -0300

    Codigo refatorado e alteração de id's

--- resources/views/components/icons/progress_bar.blade.php
+++ resources/views/components/icons/progress_bar.blade.php
@@ -5,27 +5,25 @@
     background: linear-gradient(to bottom, #ffffff, #d6e7ea);
     box-shadow: 0 0.2rem 0 #78a4a8;
 }
-#myBar{
+#barraProgresso{
 	font-weight: bold;
 	background-color: #b72626 !important;
 }
-#interatividade :nth-child(2), #nivel_interatividade{
+#barra :nth-child(2), #interatividade{
     font-size: x-large;
     color:#101728;
-}
-#nivel_interatividade {
     text-transform: uppercase;
 }
 </style>
 
 <div id="barra" class="transparencia">
     <div class="progress barra_progresso w-50 mb-2">
-        <div id="myBar" class="progress-bar" role="progressbar" style="width:0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+        <div id="barraProgresso" class="progress-bar" role="progressbar" style="width:0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <span id="interatividade" class="justify-content-center row">
         <p id="count"></p>
         <p class="pr-3"> Nível de Interatividade: </p>
-        <p id="nivel_interatividade" class="font-weight-bold"></p>
+        <p id="classificacaoInteratividadeBarra" class="font-weight-bold"></p>
     </span>
 </div>
