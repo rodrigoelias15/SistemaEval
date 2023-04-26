88a937353e101017e3a89d0e8955b58d2ad172f3
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-04-26 20:38:06 -0300

    Atualizada versão do sistema

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -47,7 +47,7 @@ Login
             </form>
             <div class="text-right" style="font-style: italic; font-size: 14px">
                 <span>
-                    v1.22.0
+                    v1.23.1
                 </span>
             </div>
         </div>
