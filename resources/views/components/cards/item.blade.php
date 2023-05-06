06a6c4fa88a5d28ee1dc57c334ab53642955c1f4
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-05-06 19:19:52 -0300

    Exibição do item digital melhorada

--- resources/views/components/cards/item.blade.php
+++ resources/views/components/cards/item.blade.php
@@ -7,51 +7,41 @@
                 {{ $item->nome_item_digital }}
                 <span class="fa fa-building-columns"></span>
             </h1>
-            
-            <p>{{$item->escolaridade_item_digital}} : {{$item->area_item_digital}}</p>
-       
-            <img id="imagem_item_digital_{{$item->id}}"
-                src="{{ url('Imagens/'.$item->imagem_item_digital) }}"
-                onerror="this.onerror=null; this.src='img/graypanel.png'" 
-                class="imagem_item_digital"
-                data-zoom-image="{{ url('Imagens/'.$item->imagem_item_digital) }}"
-                alt="Item Digital {{$item->id}}"
-                style="width:100%;
-                    height:100%;
-                    object-fit:cover;
-                    margin-bottom:1rem;"
-            >
-            <span class="mt-2 block text-gray-400 text-xs">
-                {{$item->url_item_digital}}
-            </span>
+            <img id="imagem_item_digital_{{ $item->id }}" src="{{ url('Imagens/' . $item->imagem_item_digital) }}"
+                onerror="this.onerror=null; this.src='img/graypanel.png'" class="imagem_item_digital"
+                data-zoom-image="{{ url('Imagens/' . $item->imagem_item_digital) }}"
+                alt="Item Digital {{ $item->id }}"
+                style="width:100%; height:100%; object-fit:cover; margin-bottom:1rem;">
         </div>
     </header>
 
-    <div class="card-body text-sm mt-2 space-y-4">
-        @if( $item->item_multidisciplinar  == 'Sim')
-            <p> <b>-Item multidisciplinar-</b></p>
+    <div class="card-body text-sm space-y-4">
+        <p><b> Grau/Escolaridade:</b> {{ $item->escolaridade_item_digital }}</p>
+        <p><b> Área/Disciplina:</b> {{ $item->area_item_digital }}</p>
+        <span class="mt-2 block text-gray-400 text-xs">
+            <b>URL do Item Digital:</b> {{ $item->url_item_digital }}
+        </span>
+        @if ($item->item_multidisciplinar == 'Sim')
+            <p><b> Item multidisciplinar </b></p>
         @endif
-        <p> <b>Descrição do Item Digital:</b> {{$item->descricao_item_digital}}</p>
-        
-        <div class="mt-2">
-            <p> <b>Item armazenado na plataforma:</b> {{$item->plataforma_item_digital}}
-            <p> <b>Responsável:</b> {{$item->instituicao_plataforma}}</p>
-        </div>
-        
-        <div class="mt-2">
-            @if( $item->item_utilizado_larga_escala  == 'Sim')
-                <p> <b>-Utilizado em larga escala-</b></p>
-                <p> <b>{{$item->nome_instituicao}}, {{$item->ano_item_digital}}</b> </p>
-                <p> <b>Avaliação em que foi utilizado:</b> {{$item->nome_avaliacao_item_digital}}</p>
-            @endif
-        </div>
+        <p><b>Descrição do Item Digital:</b> {{ $item->descricao_item_digital }}</p>
+
+        <p><b>Item armazenado na plataforma:</b> {{ $item->plataforma_item_digital }}
+        <p><b>Responsável:</b> {{ $item->instituicao_plataforma }}</p>
+
+        @if ($item->item_utilizado_larga_escala == 'Sim')
+            <p><b>Item utilizado em larga escala:</b> Sim</p>
+            <p><b>Instituição responsável pela avaliação:</b> {{ $item->nome_instituicao }}</p>
+            <p><b>Ano da avaliação: </b>{{ $item->ano_item_digital }} </p>
+            <p><b>Avaliação em que foi utilizado:</b> {{ $item->nome_avaliacao_item_digital }}</p>
+        @endif
     </div>
-    
+
     <footer>
-        <form class="text-center mb-3" method="post" action="/cadastroitemdigital/remover/{{$item->id}}" onsubmit="return confirm('Deseja excluir o Item Digital {{$item->nome_item_digital}}?')">
+        <form class="text-center mb-3" method="POST" action="/cadastroitemdigital/remover/{{ $item->id }}"
+            onsubmit="return confirm('Deseja excluir o Item Digital {{ $item->nome_item_digital }}?')">
             @csrf
             <button class="btn btn-danger px-4" type="submit">Excluir</button>
         </form>
     </footer>
 </x-cards.template>
-
