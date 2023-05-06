0e1ad12d8ad6449c12647c5e49cfba9d53734f97
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2023-05-06 19:21:00 -0300

    Alteração do layout e adição de imagem no relatório para download

--- resources/views/relatorio/relatorioDownload.blade.php
+++ resources/views/relatorio/relatorioDownload.blade.php
@@ -1,105 +1,169 @@
+{{-- Relatório para download --}}
 
-{{-- Criação de relatório para download --}}
+<?php
+
+function converteImagemBase64($caminhoImagem)
+{
+    $string = file_get_contents($caminhoImagem);
+    $dadosImagem = base64_encode($string);
+    $uri = 'data:image/jpeg;base64,' . $dadosImagem;
+    return $uri;
+}
+
+?>
 
 <body>
-    <p><span class="cabecalho"> Título do Item: </span> {{$dadosQuestionario->nome_item_digital_questionario}} </p>
-    <p><span class="cabecalho"> Nome do Avaliador: </span> {{$dadosQuestionario->nome_avaliador_questionario}} </p>
-    <p><span class="cabecalho"> Data da Avaliação: </span> {{ (new DateTime($dadosQuestionario->data_item_digital_questionario))->format('d/m/Y')}} </p>
-    <p><span class="cabecalho"> Nível Interatividade: </span> {{$dadosQuestionario->interatividade}} </p>
-    <p><span class="cabecalho"> Classificação da Interatividade: </span> {{$dadosQuestionario->classificacaoInteratividade}} </p>
 
-    <section id="indicador1">
-        <h1 class="indicador">Indicador 1: Textos e elementos gráficos</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item1 ?></p>                    
-    </section>
+    <img src="{{ converteImagemBase64('img/caedlogo.png') }}" alt="Logo">
 
-    <section id="indicador2">
-        <h1 class="indicador">Indicador 2: Espaço/meio de interação</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item2 ?></p>
-    </section>
+    <h1 class="centraliza">Relatório de Avaliação do Item Digital</h1>
 
-    <section id="indicador3">
-        <h1 class="indicador">Indicador 3: Comportamento dos elementos no espaço</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item3 ?></p>                    
-    </section>
+    <fieldset>
+        <p><span class="cabecalho"> Item Digital: </span> {{ $dadosQuestionario->nome_item_digital_questionario }}
+        </p>
+        <p><span class="cabecalho"> Nome do Avaliador: </span> {{ $dadosQuestionario->nome_avaliador_questionario }}
+        </p>
+        <p><span class="cabecalho"> Data da Avaliação: </span>
+            {{ (new DateTime($dadosQuestionario->data_item_digital_questionario))->format('d/m/Y') }} </p>
+        <p><span class="cabecalho"> Nível de Interatividade: </span><span
+                class="class_interatividade">{{ $dadosQuestionario->interatividade }}/3.00 </span></p>
+        <span class="cabecalho"> Classificação da Interatividade: </span> <span class="class_interatividade">
+            {{ $dadosQuestionario->classificacaoInteratividade }} </span>
+    </fieldset>
+    <fieldset>
+        <section id="indicador1">
+            <h1 class="indicador">Indicador 1: Textos e elementos gráficos</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item1; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador4">
-        <h1 class="indicador">Indicador 4: Tempo</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item4 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador2">
+            <h1 class="indicador">Indicador 2: Espaço/meio de interação</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item2; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador5">
-        <h1 class="indicador">Indicador 5: Movimento/liberdade do usuário no item</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item5 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador3">
+            <h1 class="indicador">Indicador 3: Comportamento dos elementos no espaço</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item3; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador6">
-        <h1 class="indicador">Indicador 6: Suporte e feedback</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item6 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador4">
+            <h1 class="indicador">Indicador 4: Tempo</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item4; ?></span>
+        </section>
+    </fieldset>
 
-    <div class="page-break"></div>
+    <fieldset>
+        <section id="indicador5">
+            <h1 class="indicador">Indicador 5: Movimento/liberdade do usuário no item</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item5; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador7">
-        <h1 class="indicador">Indicador 7: Diálogo entre usuários e conteúdo</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item7 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador6">
+            <h1 class="indicador">Indicador 6: Suporte e feedback</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item6; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador8">
-        <h1 class="indicador">Indicador 8: Flexibilidade e Abertura</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item8 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador7">
+            <h1 class="indicador">Indicador 7: Diálogo entre usuários e conteúdo</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item7; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador9">
-        <h1 class="indicador">Indicador 9: Integração Inter-áreas</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item9 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador8">
+            <h1 class="indicador">Indicador 8: Flexibilidade e Abertura</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item8; ?></span>
+        </section>
+    </fieldset>
 
-    <section id="indicador10">
-        <h1 class="indicador">Indicador 10: Integração colaborativa</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item10 ?></p>                    
-    </section>
-
-    <section id="indicador11">
-        <h1 class="indicador">Indicador 11: Permutabilidade</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item11 ?></p>                    
-    </section>
-
-    <section id="indicador12">
-        <h1 class="indicador">Indicador 12: Intervenção</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item12 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador9">
+            <h1 class="indicador">Indicador 9: Integração Inter-áreas</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item9; ?></span>
+        </section>
+    </fieldset>
     
-    <section id="indicador13">
-        <h1 class="indicador">Indicador 13: Bidirecionalidade</h1>
-        <h2 class="criterio">Critério escolhido:</h2>                    
-        <p><?php echo $dadosQuestionario->questionario_item13 ?></p>                    
-    </section>
+    <fieldset>
+        <section id="indicador10">
+            <h1 class="indicador">Indicador 10: Integração colaborativa</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item10; ?></span>
+        </section>
+    </fieldset>
+    
+    <div class="page-break"></div>
+    
+    <fieldset>
+        <section id="indicador11">
+            <h1 class="indicador">Indicador 11: Permutabilidade</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item11; ?></span>
+        </section>
+    </fieldset>
+
+    <fieldset>
+        <section id="indicador12">
+            <h1 class="indicador">Indicador 12: Intervenção</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item12; ?></span>
+        </section>
+    </fieldset>
+
+    <fieldset>
+        <section id="indicador13">
+            <h1 class="indicador">Indicador 13: Bidirecionalidade</h1>
+            <h2 class="criterio">Critério escolhido:</h2>
+            <span><?php echo $dadosQuestionario->questionario_item13; ?></span>
+        </section>
+    </fieldset>
 </body>
 
 <style>
+    img {
+        max-width: 20%;
+    }
+
     .page-break {
         page-break-after: always;
     }
-    .indicador{
+
+    .indicador {
         font-size: 16px;
     }
-    .criterio{
+
+    .criterio {
         font-size: 14px;
     }
-    .cabecalho{
+
+    .cabecalho {
         font-weight: bold;
+    }
+
+    .class_interatividade {
+        color: red;
+        font-weight: bold;
+    }
+
+    .centraliza {
+        text-align: center
     }
 </style>
